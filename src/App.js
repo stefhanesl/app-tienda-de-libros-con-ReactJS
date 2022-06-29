6694debb40721792fbb5f2d81f099d3179b9623e
stefhanesl <stefhanesalguero@gmail.com>
2022-06-28 21:13:46 -0700

    tienda de libros con React

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import './App.css';
+import Menu from './Menu';
+import List from './List';
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      books:[
+        {id:0, rating:4, title:'Rayuela', image: 'libro0.jpg'},
+        {id:1, rating:2, title:'Yo antes de ti ', image: 'libro1.jpg'},
+        {id:2, rating:3, title:'El libro más lindo del mundo', image: 'libro2.jpg'},
+        {id:3, rating:2, title:'La amabilidad me hace más fuerte', image: 'libro3.jpg'},
+        {id:4, rating:4, title:'12 SECRETOS DE LAS MENTES BRILLANTES', image: 'libro4.jpg'},
+        {id:5, rating:1, title:'Más allá del invierno', image: 'libro5.jpg'},
+        {id:6, rating:2, title:'La paciente silenciosa', image: 'libro6.jpg'},
+        {id:7, rating:5, title:'Más allá del bien y del mal', image: 'libro7.jpg'}
+      ],
+      copyBooks: []
+    };
+
+    this.onSearch = this.onSearch.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.remove = this.remove.bind(this);
+    this.updateRating = this.updateRating.bind(this);
+  }
+
+  initBooks(){
+    //this.setState({copyBooks: [...this.state.books]});
+    this.setState((state,props) => ({
+      copyBooks: [...state.books]
+    }));
+  }
+
+  componentDidMount(){
+    this.initBooks();
+  }
+
+  onSearch(query){
+    if(query === ''){
+      this.setState({copyBooks: [...this.state.books]});
+    }else{
+
+      const temp = [...this.state.books];
+      var res = [];
+      temp.forEach(item =>{
+        if(item.title.toLowerCase().indexOf(query) > -1){
+          res.push(item);
+        }
+      });
+    
+      this.setState({copyBooks: [...res]});
+    }
+  }
+
+  addItem(item){
+    var temp = [...this.state.books];
+    const id = temp[temp.length-1].id + 1;
+    item['id'] = id;
+    temp.push(item);
+    this.setState({books: [...temp]});
+    this.initBooks();
+  }
+
+  remove(id){
+    var temp = [...this.state.books];
+    const res = temp.filter(item => item.id != id);
+    this.setState({books: [...res]});
+    this.initBooks();
+  }
+
+  updateRating(item){
+    var temp = [...this.state.books];
+    const index = temp.findIndex(x => x.id === item.id);
+    temp[index].title = item.title;
+    temp[index].image = item.image;
+    temp[index].rating = item.rating;
+
+    this.setState({books: [...temp]});
+    this.initBooks();
+  }
+
+  render(){
+    return (
+      <div className="app">
+        <Menu onsearch={this.onSearch} onadd={this.addItem} />
+        <List className="list" items={this.state.copyBooks} onremove={this.remove} onupdaterating={this.updateRating} />
+      </div>
+    );
+  }
+}
+
+export default App;
